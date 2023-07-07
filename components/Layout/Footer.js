423dbdcb908c84a014bc51ad7dda23bf75fd75d0
akumar <akumar@comtronusa.com>
2023-07-07 02:19:46 -0400

    added all files

---
+++ components/Layout/Footer.js
@@ -0,0 +1,106 @@
+import React from "react";
+import LogoVPN from "../../public/assets/sadibazar.png";
+
+import Facebook from "../../public/assets/Icon/facebook.svg";
+import Twitter from "../../public/assets/Icon/twitter.svg";
+import Instagram from "../../public/assets/Icon/instagram.svg";
+import Image from "next/image";
+const Footer = () => {
+  return (
+    <div className="bg-white-300 pt-44 pb-24">
+      <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
+        <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
+          <Image src={LogoVPN} width={250} height={80} alt="logo" />
+
+          <p className="mb-4">
+            Welcome to{" "}
+            <strong className="font-medium text-blue-600">Sadibazar</strong>{" "}
+            your one-stop-shop for all your wedding needs! We understand that
+            planning a wedding can be stressful, time-consuming, and expensive.
+            That's why we're here to make the process as seamless and affordable
+            as possible. At Sadibazar, we believe that every couple deserves to
+            have their dream wedding, no matter their budget. That's why we work
+            with some of the best vendors in the industry to provide you with
+            high-quality services at competitive prices. Our team of wedding
+            experts is here to guide you every step of the way, ensuring that
+            your wedding day is everything you've ever wanted.
+            <br />
+            So why wait? Start planning your perfect wedding today with
+            Sadibazar.
+          </p>
+          <div className="flex w-full mt-2 mb-8 -mx-2">
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Facebook className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Twitter className="h-6 w-6" />
+            </div>
+            <div className="mx-2 bg-white-500 rounded-full items-center justify-center flex p-2 shadow-md">
+              <Instagram className="h-6 w-6" />
+            </div>
+          </div>
+          <p className="text-gray-400">
+            Copyright ©{new Date().getFullYear()}- Sadibazar -A Product Of
+            Alkaaman Event Management PVT.LTD All Rights Reserved
+          </p>
+        </div>
+        <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
+          <ul className="text-black-500 ">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Download{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Pricing{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Locations{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Server{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Countries{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Blog{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
+          <ul className="text-black-500">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              FAQ{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Tutorials{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              About Us{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Privacy Policy{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Terms of Service{" "}
+            </li>
+          </ul>
+        </div>
+        <div className="row-span-2 sm:col-span-2 sm:col-start-11 sm:col-end-13 flex flex-col">
+          <p className="text-black-600 mb-4 font-medium text-lg">Earn Money</p>
+          <ul className="text-black-500">
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Affiliate{" "}
+            </li>
+            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
+              Become Partner{" "}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
